e666929ad32b6dc5553381bdc9d56bc0b2ce4a55
Joshua Gregory <yoshig5114@gmail.com>
2023-06-02 14:34:04 -0400

    finished

--- app.js
+++ app.js
@@ -14,11 +14,25 @@ function getList() {
     }, 10);
   });
 }
-
+let errorP = document.getElementById("error");
+let unorderedList = document.querySelector("#list");
 // TODO: Handle the resolved or rejected states of the promise
+getList()
+  
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
+.then((hobbits)=>{
+  console.log(hobbits);
+  for (let hobbit of hobbits){
+    const li = document.createElement('li');
+    li.textContent = hobbit;
+    unorderedList.appendChild(li);
+  }
+})
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
+.catch((err)=>{
+  console.log(err);
+  errorP.textContent = err.message;
+})
